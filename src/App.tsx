dfa14a0f7c7ddfe8f819757fdc4d5a55102387ca
Cody Condon <codycondon@codys-air.home>
2024-02-06 11:13:55 -0500

    add first part of layout:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
+import SignInForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignupForm";
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
         <Route path="sign-in" element={<SignInForm />} />
-
+        <Route path="sign-in" element={<SignUpForm />} />
         {/* private routes */}
-        <Route index element={Home/>}/>
+        <Route index element={<Home />} />
       </Routes>
     </main>
   );
